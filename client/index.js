815d3557dc0288ee41386201a0d6019ac032f979
jbjw <github@jbjw.com>
2018-05-01 05:47:41 -0700

    auto-save

--- client/index.js
+++ client/index.js
@@ -2,11 +2,9 @@
 
 "use strict"
 
-var myList = document.querySelector('ul');
+var myList = document.querySelector("ul")
 
-var myRequest = new Request('products.json');
-
-fetch(myRequest)
+fetch("myRequest")
 	.then(function(response) { return response.json(); })
 	.then(function(data) {
 		for (var i = 0; i < data.products.length; i++) {
